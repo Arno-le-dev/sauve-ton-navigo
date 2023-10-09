7549cc95322163ed08b5001fdab1c41c1695abae
Arno Bruno <61425813+Arno-le-dev@users.noreply.github.com>
2023-10-09 21:44:08 +0200

    feat: Initial commit

---
+++ sauve-ton-navigo/src/main/java/com/ratp/sauvetonnavigo/SauveTonNavigoApplication.java
@@ -0,0 +1,13 @@
+package com.ratp.sauvetonnavigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SauveTonNavigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SauveTonNavigoApplication.class, args);
+	}
+
+}
---
+++ sauve-ton-navigo/src/test/java/com/ratp/sauvetonnavigo/SauveTonNavigoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ratp.sauvetonnavigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SauveTonNavigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
